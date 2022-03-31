feddc882d182fcf094fc014fd4cff0decc1407c2
zhouyun0922 <zhoubiao0922@126.com>
2022-03-31 17:19:51 +0800

    conftest.py

---
+++ nlx_Mealordering_Store/conftest.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''
+@Project ：nlx_Mealordering_Store 
+@File    ：conftest.py
+@Author  ：周云
+@Date    ：2022/3/31 17:03 
+'''
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
