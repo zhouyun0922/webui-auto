7222b086d2fa35b3e2b00aa6b3bed910541eb216
zhouyun0922 <zhoubiao0922@126.com>
2022-04-06 14:19:52 +0800

    updatenew

--- nlx_Mealordering_Store/base/base_util.py
+++ nlx_Mealordering_Store/base/base_util.py
@@ -16,7 +16,6 @@ class Baseutil(object):
 
 	def setup(self) -> None:
 		global driver
-		global options
 		self.options = Options()
 		self.options.add_argument('--headless')
 		self.options.add_argument('--no-sandbox')
@@ -27,7 +26,7 @@ class Baseutil(object):
 		#self.options.add_experimental_option('useAutomationExtension', False)
 		#self.options.binary_location = r'/usr/bin/google-chrome-stable'
 		# 打开浏览器
-		self.driver = webdriver.Chrome(executable_path=r'/usr/bin/chromedriver', options=self.options)
+		self.driver = webdriver.Chrome(executable_path=r'/usr/bin/chromedriver', chrome_options=self.options)
 		time.sleep(2)
 		# 加载网页
 		self.driver.get("http://192.168.2.92:8083/login")
