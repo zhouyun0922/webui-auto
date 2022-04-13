ed3ead96b60d343f27cd391959c14a4c472f4de0
zhouyun0922 <zhoubiao0922@126.com>
2022-04-13 10:37:56 +0800

    updatewebshop

--- nlx_webshop/base/base_util.py
+++ nlx_webshop/base/base_util.py
@@ -5,18 +5,31 @@
 #encoding=UTF-8
 import time
 import unittest
+import allure
 from selenium import webdriver
 
 class base_util(unittest.TestCase):
     def setUp(self) -> None:
         global driver
+        self.options = Options()
+        # self.options.add_argument('--headless')
+        self.options.add_argument('--no-sandbox')
+        self.options.add_argument('--disable-gpu')
+        self.options.add_argument('--disable-dev-shm-usage')
+        self.options.binary_location = '/usr/bin/google-chrome-stable'
+        # self.options.add_argument('--window-size=1420,1080')
+        # self.options.add_argument("--disable-notifications")
+        # self.options.add_experimental_option('useAutomationExtension', False)
         # 打开浏览器
-        self.driver = webdriver.Chrome()
+        self.driver = webdriver.Chrome(executable_path=r'/usr/bin/chromedriver', chrome_options=self.options)
+        time.sleep(2)
         # 加载网页
         self.driver.get("http://192.168.2.92:8098/login")
+        time.sleep(1)
         # 全屏显示
         self.driver.maximize_window()
-        time.sleep(1)
 
     def tearDown(self) -> None:
-        pass
+        time.sleep(1)
+        img = self.driver.get_screenshot_as_png()
+        allure.attach(img, "用例执行截图", allure.attachment_type.PNG)
--- nlx_webshop/pageobject/uplaod.py
+++ nlx_webshop/pageobject/uplaod.py
@@ -1,8 +1,19 @@
 #encoding=UTF-8
-import pywinauto
-import unittest
+#import pywinauto
+#import unittest
+import time
 from pywinauto.keyboard import send_keys
 def uplaod_file(filename):
+    # 选择文件
+    send_keys(r"/home/zhouyun")
+    time.sleep(2)
+    # 确定
+    send_keys('{ENTER}')
+    #上传文件
+    send_keys(filename)
+    # 确定
+    send_keys('{ENTER}')
+    '''
     # 使用pywinauto来选择文件
     app = pywinauto.Desktop()
     # 选择文件上传的窗口
@@ -11,7 +22,7 @@ def uplaod_file(filename):
     # 选择文件地址输入框，点击**
     dlg["Toolbar3"].click()
     # 键盘输入上传文件的路径
-    send_keys(r"C:\Users\周云\Desktop\测试图片")
+    send_keys(r'C:/Users/周云/Desktop/测试图片')
     # 键盘输入回车，打开该路径
     send_keys("{VK_RETURN}")
 
@@ -20,6 +31,7 @@ def uplaod_file(filename):
 
     # 点击打开
     dlg["打开(&O)"].click()
+    '''
 '''
 if __name__ == "__main__":
     filename = "1.jpg"
